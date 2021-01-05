23e1870d3b84b5a9b34b8458facb70838b7366e1
MrLaam <jordanlam60@gmail.com>
2021-01-05 16:49:51 +0000

    Changed footer colour and added copyright text. Added 'scroll to top' button with animation

--- LearnerBikes/wwwroot/js/site.js
+++ LearnerBikes/wwwroot/js/site.js
@@ -10,3 +10,28 @@
 function applyFilters() {
     document.getElementById("bikeFilterForm").submit();
 }
+
+
+var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+var rootElement = document.documentElement;
+
+function scrollToTop() {
+    rootElement.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+}
+
+scrollToTopBtn.addEventListener("click", scrollToTop);
+
+function handleScroll() {
+    var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight
+    if ((rootElement.scrollTop / scrollTotal) > 0.80) {
+        scrollToTopBtn.classList.add("showBtn")
+    } else {
+        scrollToTopBtn.classList.remove("showBtn")
+    }
+}
+
+
+document.addEventListener("scroll", handleScroll)
