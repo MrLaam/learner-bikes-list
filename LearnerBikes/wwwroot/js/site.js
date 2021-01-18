eb3b892899000e76521fb5e8ebebdf55c57c06a0
MrLaam <jordanlam60@gmail.com>
2021-01-18 15:06:56 +0000

    Added mobile navigation functionality to hide menu when empty space below is clicked

--- LearnerBikes/wwwroot/js/site.js
+++ LearnerBikes/wwwroot/js/site.js
@@ -1,10 +1,26 @@
-﻿/*Open and close mobile filter and navigation menus*/
-function toggleDisplay(id) {
+﻿/*Open and close mobile navigation menu*/
+function toggleNavbar(id) {
     var filterSection = document.getElementById(id);
+    var navbarBlankSpace = document.getElementById('navbarBlankSpace');
     if (filterSection.style.display === "block") {
         filterSection.style.display = "none";
+        navbarBlankSpace.style.display = "none";
+        document.getElementsByTagName("body")[0].style = "overflow: auto"
     } else {
         filterSection.style.display = "block";
+        navbarBlankSpace.style.display = "block";
+        document.getElementsByTagName("body")[0].style = "overflow: hidden";
+    }
+}
+/**/
+
+/*Toggle mobile filter menu*/
+function toggleFilter(id) {
+    var filter = document.getElementById(id);
+    if (filter.style.display === "block") {
+        filter.style.display = "none";
+    } else {
+        filter.style.display = "block";
     }
 }
 /**/
